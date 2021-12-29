0d5758d309911a3447d0c618b31c75eab096cc69
Mudit <muditmnr@gmail.com>
2021-12-29 16:11:27 +0530

    feedback

---
+++ feedback/script.js
@@ -0,0 +1,6 @@
+function hideshow(){
+    let nevresp = document.getElementById('nevbar-resp');
+    let visibal = document.getElementById('visibality');
+    nevresp.classList.toggle("nevbar-resp");
+    visibal.classList.toggle("visibality");
+  }
